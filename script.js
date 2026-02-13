f864d659903b0e97252ec3bb17a360a8adfa4674
TheMarinPopah <132628734+TheMarinPopah@users.noreply.github.com>
2026-02-12 19:51:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  const dpr = window.devicePixelRatio || 1;
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  canvas.width = width * dpr;
+  canvas.height = height * dpr;
+
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+
+  ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "white";
+  ctx.font = "40px Arial";
+  ctx.fillText("Separate CSS & JS Working!", 50, 100);
+
+  requestAnimationFrame(draw);
+}
+
+draw();
